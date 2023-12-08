058aca2f8648a244af8a7f6aabbd6bfa7396b962
unknown <phero919@gmail.com>
2023-12-08 10:24:37 +0000

    simple calculator

--- calculator.js
+++ calculator.js
@@ -1,21 +1,21 @@
 function add() {
-    var num1 = parseFloat(document.getElementById("num1").value); // Get first number from input
-    var num2 = parseFloat(document.getElementById("num2").value); // Get second number from input
-    var result = num1 + num2; // Add the numbers
-    document.getElementById("result").innerText = result; // Display the result
+    var num1 = parseFloat(document.getElementById("num1").value); 
+    var num2 = parseFloat(document.getElementById("num2").value); 
+    var result = num1 + num2; 
+    document.getElementById("result").innerText = result; 
 }
 
 function subtract() {
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
-    var result = num1 - num2; // Subtract the numbers
+    var result = num1 - num2; 
     document.getElementById("result").innerText = result;
 }
 
 function multiply() {
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
-    var result = num1 * num2; // Multiply the numbers
+    var result = num1 * num2; 
     document.getElementById("result").innerText = result;
 }
 
@@ -25,7 +25,7 @@ function divide() {
     if (num2 === 0) {
         document.getElementById("result").innerText = "Cannot divide by zero";
     } else {
-        var result = num1 / num2; // Divide the numbers
+        var result = num1 / num2; 
         document.getElementById("result").innerText = result;
     }
 }
